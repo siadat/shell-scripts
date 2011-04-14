c85360554b6ab5cde78e28f734e43542868d6424
Sina Siadat <siadat@gmail.com>
2011-04-15 02:26:03 +0430

    updated date.sh: removed ZSH dependency
    Removed ZSH dependency. Now it only depends on BASH for colored output.
    Also some change in output to highlight the local time zone.

--- date/date.sh
+++ date/date.sh
@@ -1,41 +1,39 @@
-#!/usr/bin/zsh
-# Get time and date in the cities I care about
+#! /bin/bash
+# What: Get time and date in the cities I care about, high light local zone.
 # Usage: date.sh
 # Suggestions: watch --color -n 1 date.sh
+# Requires: bash
 
-# NOTE: uses zsh
-
+bldred='\e[1;31m' # Red
+bldgrn='\e[1;32m' # Green
 bldylw='\e[1;33m' # Yellow
 bldwht='\e[1;37m' # White
 txtrst='\e[0m'    # Text Reset
 
+LOCAL_TZ=`date +"%Z"`
+
 getdate() { 
-  date +"%:z ${bldwht}%T${txtrst} %a %F ${bldylw}%Z${txtrst}" 
+  export TZ=$1
+  if [ $LOCAL_TZ != `date +"%Z"` ]; then
+    echo -e "`date +"${bldwht}%T${txtrst} %a %F %:z %Z"`"
+  else                                           
+    echo -e "`date +"${bldylw}%T${txtrst} ${bldylw}%a %F ${txtrst}%:z %Z"`"
+  fi
 }
 
-# http://en.wikipedia.org/wiki/List_of_tz_database_time_zones
-export TZ=Asia/Tehran
-IRA=`getdate`
-export TZ=Europe/London
-LON=`getdate`
-export TZ=America/New_York
-NYC=`getdate`
-export TZ=America/Chicago
-CHI=`getdate`
-export TZ=America/Los_Angeles
-LOS=`getdate`
-export TZ=Asia/Singapore
-SIN=`getdate`
-export TZ=Australia/Sydney
-SYD=`getdate`
+# See also: http://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+IRA=`getdate "Asia/Tehran"`
+LON=`getdate "Europe/London"`
+NYC=`getdate "America/New_York"`
+CHI=`getdate "America/Chicago"`
+LOS=`getdate "America/Los_Angeles"`
+SIN=`getdate "Asia/Singapore"`
+SYD=`getdate "Australia/Sydney"`
 
 echo "     Sydney ${SYD}"
 echo "  Singapore ${SIN}"
 echo "     Tehran ${IRA}"
 echo "     London ${LON}"
-echo "   New York ${NYC} (Eastern) = Quebec"
-echo "    Chicago ${CHI} (Central)"
-echo "Los Angeles ${LOS} (Pacific)"
-
-# not needed:
-unset TZ
+echo "   New York ${NYC}" # (Eastern and Quebec)"
+echo "    Chicago ${CHI}" # (Central)"
+echo "Los Angeles ${LOS}" # (Pacific)"
