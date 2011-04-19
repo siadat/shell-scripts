666feae089b3c9adb622280a14984f4c65e282a1
Sina Siadat <siadat@gmail.com>
2011-04-20 00:05:18 +0430

    fixed 'read' in replace.sh to work with /big/sh

--- replace/replace.sh
+++ replace/replace.sh
@@ -3,5 +3,6 @@ echo "Find regexp: \<${1}\>"
 echo "Replace with: ${2}"
 echo "In these files:"
 find -type f |xargs grep --color=auto "\<${1}\>"
-read -p "Press enter to confirm..."
+echo "Press enter to confirm..."
+read user_enter
 find -type f |xargs grep -l "\<${1}\>" |xargs sed -i -e "s/\<${1}\>/${2}/g"
